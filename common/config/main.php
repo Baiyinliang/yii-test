55fa5b2518354548aaef1303bb11d9bfdc7b994d
Evan <bailiwen21@gmail.com>
2019-07-05 11:00:54 +0800

    调整路由

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,9 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
     ],
 ];
